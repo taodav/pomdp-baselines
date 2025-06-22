bd437dfd87bb7901a669588ce708d3ebcf5cff31
Ruo Yu Tao <ruoyutao@gmail.com>
2025-06-22 12:08:19 -0700

    update time_test

--- time_test.py
+++ time_test.py
@@ -1,20 +1,21 @@
 import gym
 from time import time
+from tqdm import trange
 
 if __name__ == "__main__":
     import envs
     steps = int(1e7)
 
     t = time()
-    env_name = "Walker-V-v0"
+    env_name = "WalkerBLT-V-v0"
     env = gym.make(env_name)
     obs = env.reset()
     done = False
     step = 0
-    for i in range(steps):
+    for i in trange(steps):
         next_obs, rew, done, info = env.step(env.action_space.sample())
         step += 1
-        print(step, done, info)
+        # print(step, done, info)
 
     new_t = time()
     total_runtime = new_t - t
